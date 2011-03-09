10e44d10821fc0eb5d6812aede6a43441060cec6
Morikawa, Masaru <morikawa@digion.com>
2011-03-09 14:15:42 +0900

    auto-complete

--- init.el
+++ init.el
@@ -36,3 +36,21 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/auto-install"))
 (setq explicit-bash-args '("--login" "-i"))
 (require 'setup-cygwin)
+
+(autoload 'cycle-buffer "cycle-buffer" "Cycle forward." t)
+(autoload 'cycle-buffer-backward "cycle-buffer" "Cycle backward." t)
+(autoload 'cycle-buffer-permissive "cycle-buffer" "Cycle forward allowing *buffers*." t)
+(autoload 'cycle-buffer-backward-permissive "cycle-buffer" "Cycle backward allowing *buffers*." t)
+(autoload 'cycle-buffer-toggle-interesting "cycle-buffer" "Toggle if this buffer will be considered." t)
+(global-set-key [(f9)]        'cycle-buffer-backward)
+(global-set-key [(f10)]       'cycle-buffer)
+(global-set-key [(shift f9)]  'cycle-buffer-backward-permissive)
+(global-set-key [(shift f10)] 'cycle-buffer-permissive)
+
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp//ac-dict")
+(ac-config-default)
+
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
