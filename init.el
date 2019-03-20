40b76b3e8996432997066bad8076c15d0050a10b
Masaru Itoh <masarito@yahoo-corp.jp>
2019-03-20 11:56:53 +0900

    typescript, plantuml

--- init.el
+++ init.el
@@ -1,9 +1,45 @@
+(setq use-dialog-box nil)
+(tooltip-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+(setq scroll-margin 1
+      scroll-conservatively 1
+      mouse-wheel-scroll-amount '(1))
+
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
+
+(global-auto-revert-mode)
+(setq whitespace-style '(face trailing))
+(whitespace-mode 1)
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+(delete-selection-mode 1)
+
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-/") 'pop-to-mark-command)
+
+(electric-indent-mode -1)
+(add-hook 'after-change-major-mode-hook (lambda() (electric-indent-mode -1)))
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+(setq c-default-style "linux")
+(defvaralias 'c-basic-offset 'tab-width)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
 (package-initialize)
 
 (unless package-archive-contents
@@ -246,24 +282,36 @@
   (js2-mode . js2-imenu-extras-mode)
   (js2-mode . eldoc-mode))
 
-(use-package prettier-js
-  :hook (js2-mode . prettier-js-mode))
+(use-package json-mode
+  :custom
+  (json-reformat:indent-width 2)
+  (js-indent-level 2))
 
 (use-package js2-refactor
   :config (js2r-add-keybindings-with-prefix "C-c r")
   :bind (:map js2-mode-map (("C-k" . js2r-kill)))
   :hook (js2-mode . js2-refactor-mode))
 
+(use-package indium
+  :hook (js2-mode . indium-interaction-mode))
+
 (use-package typescript-mode
-  :hook (typescript-mode . eldoc-mode))
+  :custom (typescript-indent-level 2)
+  :hook
+  (typescript-mode . eldoc-mode)
+  (typescript-mode . display-line-numbers-mode))
 
 (use-package tide
   :hook
   ((js2-mode typescript-mode) . tide-setup)
   ((js2-mode typescript-mode) . tide-hl-identifier-mode))
 
-(use-package indium
-  :hook ((js2-mode typescript-mode) . indium-interaction-mode))
+(use-package prettier-js
+  :hook ((json-mode js2-mode typescript-mode) . prettier-js-mode))
+
+(use-package ts-comint
+  :bind
+  (("C-x C-e" . ts-send-last-sexp)))
 
 ;; Rust
 
@@ -282,41 +330,10 @@
 (use-package yaml-mode
   :init (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
 
-(setq c-default-style "linux")
-
-(setq use-dialog-box nil)
-(tooltip-mode -1)
-(tool-bar-mode -1)
-(toggle-scroll-bar -1)
-(setq scroll-margin 1
-      scroll-conservatively 1
-      mouse-wheel-scroll-amount '(1))
-
-(setq ring-bell-function
-      (lambda ()
-        (let ((orig-fg (face-foreground 'mode-line)))
-          (set-face-foreground 'mode-line "#F2804F")
-          (run-with-idle-timer 0.1 nil
-                               (lambda (fg) (set-face-foreground 'mode-line fg))
-                               orig-fg))))
-
-(global-auto-revert-mode)
-(setq whitespace-style '(face trailing))
-(whitespace-mode 1)
-(setq show-paren-delay 0)
-(show-paren-mode 1)
-(delete-selection-mode 1)
-
-(global-set-key (kbd "C-z") 'undo)
-(global-set-key (kbd "C-'") 'pop-to-mark-command)
-
-(setq-default indent-tabs-mode nil)
-(setq default-tab-width 2
-      c-basic-offset 2)
+(use-package plantuml-mode
+  :custom (plantuml-jar-path "~/bin/plantuml.jar"))
 
 (let ((site-init "~/.emacs.d/site-init.el"))
   (when (file-exists-p site-init)
     (load-file site-init)))
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
